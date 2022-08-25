0d6bf347d1d8a31ce0c403330c6a6994397316fd
Rahul Yadav <72229574+rahulyadav111@users.noreply.github.com>
2022-08-25 16:13:23 +0530

    Add files via upload

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CrudWithMvc.Models;
+
+
+
+namespace CrudWithMvc.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        // 1. *************RETRIEVE ALL STUDENT DETAILS ******************
+        // GET: Student
+        public ActionResult Index()
+        {
+            StudentDbHandle dbhandle = new StudentDbHandle();
+            ModelState.Clear();
+            return View(dbhandle.GetStudent());
+        }
+
+        // GET: Student
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(StudentModel smodel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    StudentDbHandle sdb = new StudentDbHandle();
+                    if (sdb.AddStudent(smodel))
+                    {
+                        ViewBag.Message = "Student Details Added Successfully";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        // POST: Student/Create
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        // 3. ************* EDIT STUDENT DETAILS ******************
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            StudentDbHandle sdb = new StudentDbHandle();
+            return View(sdb.GetStudent().Find(smodel => smodel.Id == id));
+        }
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, StudentModel smodel)
+        {
+            try
+            {
+                StudentDbHandle sdb = new StudentDbHandle();
+                sdb.UpdateDetails(smodel);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // 4. ************* DELETE STUDENT DETAILS ******************
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                StudentDbHandle sdb = new StudentDbHandle();
+                if (sdb.DeleteStudent(id))
+                {
+                    ViewBag.AlertMsg = "Student Deleted Successfully";
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/StudentModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudWithMvc.Models
+{
+    public class StudentModel
+    {
+
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "City is required.")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Address is required.")]
+        public string Address { get; set; }
+
+        [Display(Name="Mobile No")]
+        [Required(ErrorMessage ="Enter Your Mobile no")]
+        public string Mobile_no { get; set; }
+        
+    }
+}
